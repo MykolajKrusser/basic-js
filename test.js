47bd2c5cfb5648cf4b024f37dd1025660fe4c5ff
Nicolas <nicolaskrusser@gmail.com>
2019-02-10 11:01:42 +0100

    test3

--- test.js
+++ test.js
@@ -1,36 +1,29 @@
 "use strict";
-var info1 = {
-    'palnet':'Earth',
-    'living':true
-};
-var info2 = {
-    'palnet':'Earth',
-    'living':true
-};
-
-console.log(info1===info2 || info1==info2);
-
-/////////////
-
 let a = 3;
-let b = 2;
-var z = (++b + a--)%5;
-console.log(z);
-
-//////////////////
-
-let map = new Map([['1','one'],['1','one'],[1,'one']])
-console.log(map);
-
-////////////////
+let b = a++;
+let c = ++a;
 
 
-let arr = [1,2,3,4];
-let x = 0;
-for (let y = 0; y < arr.length; y++){
-    if(y % 2 == 0){
-        x += arr[y]
-        console.log(arr[y])
-    }
+
+///////////////
+
+/*
+let x = 2;
+
+for(let i=0; i<4; i++){
+    x+=2;
+    
 }
+*/
+
+////////////////////
+
+let x = 100;
+while( x >= 50 ){
+    x--
+    console.log(x)
+}
+x %= 7
+
 console.log(x)
+
