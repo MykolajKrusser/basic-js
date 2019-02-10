03d3e8204675eb422a8e03827130783d5e4422db
Nicolas <nicolaskrusser@gmail.com>
2019-02-10 09:38:23 +0100

    test2

--- test.js
+++ test.js
@@ -1,15 +1,36 @@
 "use strict";
-let a = 5;
-let b = a++;
+var info1 = {
+    'palnet':'Earth',
+    'living':true
+};
+var info2 = {
+    'palnet':'Earth',
+    'living':true
+};
+
+console.log(info1===info2 || info1==info2);
+
+/////////////
+
+let a = 3;
+let b = 2;
+var z = (++b + a--)%5;
+console.log(z);
+
+//////////////////
+
+let map = new Map([['1','one'],['1','one'],[1,'one']])
+console.log(map);
+
+////////////////
 
 
-
-let arr = [11, 22, 33, 44]
-let newAr = arr.slice(1, 2)
-
-
-
-const characters = [ 'Obi-Wan', 'Vader' ]
-const newCharacters = [...characters]
-newCharacters.push('Luke')
-console.log(aracters === newCharactersch) // !true :-(
+let arr = [1,2,3,4];
+let x = 0;
+for (let y = 0; y < arr.length; y++){
+    if(y % 2 == 0){
+        x += arr[y]
+        console.log(arr[y])
+    }
+}
+console.log(x)
