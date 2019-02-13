2cc7406a433b93313defa748a46939c9f67a8fda
Nicolas <nicolaskrusser@gmail.com>
2019-02-13 08:47:43 +0100

    tests3

--- test.js
+++ test.js
@@ -1,9 +1,60 @@
-"use strict";
+"use strict"
+function fn2(){
+    console.log( this )
+}
+fn2();
+/////
 function update(obj){
-    obj.color = 'red'
-    console.log(a)
+    console.log(obj)
 }
 let a = {color : 'blue'};
-
 update(a);
-console.log(update);
+
+//////
+
+function funk(){
+    console.log(this === window)
+}
+funk()
+
+/////
+
+var obj = {
+    get: function(){
+        return this;
+    }
+};
+
+console.log(obj.get() === obj)
+/////
+
+console.log( typeof this)
+
+/////
+
+var obj2 = {
+    get: function(){
+        console.log(this);
+    }
+};
+
+obj2.get()
+
+///////////
+
+function foo(){
+    console.log(this)
+}
+foo();
+
+////////////
+
+var foo3 = {
+    get: function () {
+            function fn(){
+                 console.log(this)
+            }
+            fn();
+        }
+}
+foo3.get();
