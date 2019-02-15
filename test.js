b6a7d84346a8683160674faf0e22ce92b2c04ffb
Nicolas <nicolaskrusser@gmail.com>
2019-02-15 12:01:32 +0100

    test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 "use strict"
-console.log(NaN == NaN);
-console.log(100 + NaN);
-console.log('100' + NaN);
-console.log(typeof NaN)
+console.log([] == []);
+console.log({} == {});
+var x = Object('xys')
+console.log(x.constructor);
