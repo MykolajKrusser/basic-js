6b1ff626b291573e623d69d4599102855f348650
Nicolas <nicolaskrusser@gmail.com>
2019-02-16 12:32:13 +0100

    quize

--- test.js
+++ test.js
@@ -1,5 +1,41 @@
 "use strict"
-console.log([] == []);
-console.log({} == {});
-var x = Object('xys')
-console.log(x.constructor);
+console.log(new String("This is a string") instanceof String);
+console.log("This is a string" instanceof String);
+console.log(typeof ([] + [] + 'foo'.split('')));
+console.log(NaN === NaN);
+
+
+var x = 0;
+function foo() {
+    return foo;
+}
+var bar = new new foo;
+console.log(bar.x);
+
+
+var bax = 1,
+    fooz = {};
+
+fooz: {
+    bax: 2;
+    baz: ++bar;
+};
+console.log(fooz.baz + fooz.bax + bax);
+
+var myArr = ['foo', 'bar', 'baz'];
+myArr[2];
+console.log('2' in myArr);
+
+var arr = [];
+arr[0]  = 'a';
+arr[1]  = 'b';
+arr.foo = 'c';
+console.log(arr.length);
+
+
+
+function foof(a, b) {
+    arguments[1] = 2;
+    console.log(b);
+}
+foof(1);
