dd40a7ac10d2606b8226bf431795f3831f7bfb8e
Nicolas <nicolaskrusser@gmail.com>
2019-02-20 17:50:14 +0100

    test34

--- test.js
+++ test.js
@@ -1,41 +1,13 @@
 "use strict"
-console.log(new String("This is a string") instanceof String);
-console.log("This is a string" instanceof String);
-console.log(typeof ([] + [] + 'foo'.split('')));
-console.log(NaN === NaN);
+var x = Math.pow(Math.floor(Math.PI), 3)
+console.log(x)
 
+//////////
+ var myNumber = [3, 6, 11, 5];
+ myNumber.sort(function(a,b){
+     return b - a;
+ })
+ console.log(myNumber)
+ //////////
 
-var x = 0;
-function foo() {
-    return foo;
-}
-var bar = new new foo;
-console.log(bar.x);
-
-
-var bax = 1,
-    fooz = {};
-
-fooz: {
-    bax: 2;
-    baz: ++bar;
-};
-console.log(fooz.baz + fooz.bax + bax);
-
-var myArr = ['foo', 'bar', 'baz'];
-myArr[2];
-console.log('2' in myArr);
-
-var arr = [];
-arr[0]  = 'a';
-arr[1]  = 'b';
-arr.foo = 'c';
-console.log(arr.length);
-
-
-
-function foof(a, b) {
-    arguments[1] = 2;
-    console.log(b);
-}
-foof(1);
+ 
