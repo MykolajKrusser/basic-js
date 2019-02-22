df4fb084e6e2c2bfb2915c689b887c08465d6414
Nicolas <nicolaskrusser@gmail.com>
2019-02-22 21:19:43 +0100

    test

--- test.js
+++ test.js
@@ -13,10 +13,30 @@ var one = {
     two: {
         x: 1,
         three: function() {
-            return this.x
+            //return this.x
         } 
     }
 }
 var go = one.two.three;
 
-console.log(one.two.three() + '' + go())
+//console.log(one.two.three() + '' + go())
+
+var arr = [2,5,2,4];
+var xy = 1;
+for(var i = 0; i < arr.length; i++){
+    if(arr[i]%2 === 0){
+        xy*=arr[i]
+        //console.log(xy)
+    }
+}
+//console.log(xy)
+
+var first = [2,5,2,4];
+var second = [...first, 6554];
+//var another = first;
+second.push(5);
+//another.push(1111)
+console.log(second)
+
+/////
+
