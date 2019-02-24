731e91fe3b0a12bb469aad9f60e3d23e144b6444
Nicolas <nicolaskrusser@gmail.com>
2019-02-24 19:01:02 +0100

    fibonachi arr and func

--- test.js
+++ test.js
@@ -1,44 +1,21 @@
+fib = function(numMax){
+  for(var fibArray = [0,1], i=0,j=1,k=0; k<numMax;i=j,j=x,k++ ){
+      x=i+j;
+      fibArray.push(x);
+  }
+  console.log(fibArray);
+}
 
-var foo = {n: 1};
-var bar = foo;
-foo.x = foo = {n: 2};
-console.log(foo.x)
+fib(10)
 
 ////
 
 
-
-const add = (a) => {
-    let sum = a;
-    const func = (b) => {
-      if (b) {
-        sum += b;
-        return func;
-      } else {
-        return sum;
-      }
-    };
-    return func;
-  };
-  
-
-  console.log(add(2)(3)())
-
-  ////
-
-  const add2 = (a) => {
-    let sum = a;
-    const func = (b) => {
-      sum += b;
-      return func;
-    };
-    func.valueOf = () => sum;
-  return func;
-  };
-  console.log(add2(2)(3));
-
-  /////
-
-  const arr = [1, [1, 2, [3, 4]], [2, 4]];
-  const flatten = (arr) => arr.reduce((flat, toFlatten) =>  flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten), []);
-      console.log(flatten)
+function fib2(n) {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fib2(n - 1) + fib2(n - 2);
+  }
+}
+console.log(fib2(6));
