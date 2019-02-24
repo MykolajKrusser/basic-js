67f1923e762a147e7ba8a573e5227b6b04da6b01
Nicolas <nicolaskrusser@gmail.com>
2019-02-24 18:34:29 +0100

    str

--- test.js
+++ test.js
@@ -1,27 +1,44 @@
-"use strict"
-var Employee = {
-    company: 'xyz'
-}
-var emp = Object.create(Employee);
-delete emp.company;
-console.log(emp)
-//////
 
-function Person(){
-    console.log(this)
-}
+var foo = {n: 1};
+var bar = foo;
+foo.x = foo = {n: 2};
+console.log(foo.x)
 
-var person = new Person();
-
-/////
+////
 
 
-function foo(){
-    this.baz = 'baz';
-    console.log(this.bar, baz)
-}
-var bar = 'bar';
-new foo()
 
-///
+const add = (a) => {
+    let sum = a;
+    const func = (b) => {
+      if (b) {
+        sum += b;
+        return func;
+      } else {
+        return sum;
+      }
+    };
+    return func;
+  };
+  
 
+  console.log(add(2)(3)())
+
+  ////
+
+  const add2 = (a) => {
+    let sum = a;
+    const func = (b) => {
+      sum += b;
+      return func;
+    };
+    func.valueOf = () => sum;
+  return func;
+  };
+  console.log(add2(2)(3));
+
+  /////
+
+  const arr = [1, [1, 2, [3, 4]], [2, 4]];
+  const flatten = (arr) => arr.reduce((flat, toFlatten) =>  flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten), []);
+      console.log(flatten)
