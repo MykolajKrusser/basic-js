0d77879950feed72450b2d89fb9f4d392fecbf82
Nicolas <nicolaskrusser@gmail.com>
2019-02-27 15:57:31 +0100

    ssss

--- test.js
+++ test.js
@@ -1,21 +1,49 @@
-fib = function(numMax){
-  for(var fibArray = [0,1], i=0,j=1,k=0; k<numMax;i=j,j=x,k++ ){
-      x=i+j;
-      fibArray.push(x);
-  }
-  console.log(fibArray);
+function evenize(a){
+  if(a % 2 == 0)
+  return a;
+  return a+1;
 }
 
-fib(10)
+var a = evenize(13)
+document.write(a)
 
-////
+
+//////////
 
 
-function fib2(n) {
-  if (n <= 1) {
-    return n;
-  } else {
-    return fib2(n - 1) + fib2(n - 2);
-  }
+
+var arr = [1,3,4,2];
+var count = 0;
+for(var x = 0; x < arr.length; x++){
+  x = arr[x];
+  count++
+  console.log(x)
 }
-console.log(fib2(6));
+console.log(count);
+
+/////
+
+
+
+let mySet = new Set();
+mySet.add(8).add(7).add(1);
+for(let v of mySet.values()){
+  console.log(mySet.has(1) + v);
+  break
+}
+
+
+//////
+ var b = 10;
+ var s = 20;
+ console.log(b + s % 3)
+
+
+ //////
+
+ var first = function(a,b){
+   return a + b
+ };
+ var second = first.bind(null, 'learn');
+ console.log(second('solo'))
+
