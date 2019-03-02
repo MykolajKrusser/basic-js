43ccb30215e6a2f015804d9f9c74b431d7c03373
Nicolas <nicolaskrusser@gmail.com>
2019-03-02 18:36:36 +0100

    qqqq

--- test.js
+++ test.js
@@ -1,32 +1,47 @@
-/*
-let f;
+let nums = [1, 2, 2, 3]; // [1,2,3]
+console.log([...new Set(nums)]);
 
-if(true){
-  let i = 1;
-  f = ()=>{
-    console.log(i)
-  }
-
-}
-console.dir(f);
-f();
-*/
 
 let f = ()=>{
-  let i = 1;
-  let j = 2;
-  return ()=>{
-    console.log(i)
-    console.log(j)
+  {
+    (function () {
+      let l = 'let';
+      var v = 'var';
+    })();
+  };
+  //console.log(v);
+  //console.log(l)
+
+}
+f();
+
+console.log(5 < 6 < 7);
+console.log(7 > 6 > 5);
+
+let b  = function() { return arguments};
+console.log(b('hi'))
+
+let a  = (...n) => { return n};
+console.log(a('hi'))
+
+let x = function(){
+  return{
+    messege: 'hi'
   }
 }
-console.dir(f())
+console.log(x())
 
-for(var i = 0; i < 3; i++){
-  ((i)=>{
-      setTimeout (()=>{
-      console.log(i)
-    }, 1000)
-  })(i);
-  
+
+let profile = {
+  name : 'qwer'
 }
+
+Object.defineProperty( profile, 'age', {
+  value: 3,
+  writable: false
+})
+profile.name = 'asaaa';
+profile.age = 5;
+console.log(profile);
+
+console.log(Math.max())
