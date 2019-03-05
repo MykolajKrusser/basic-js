f5257347eeb1b3ba06feb1289612a6b78ebcb409
Nicolas <nicolaskrusser@gmail.com>
2019-03-05 09:22:02 +0100

    tricky q

--- test.js
+++ test.js
@@ -1,33 +1,42 @@
-let arr = [
-  {
-    num: 3,
-    str: 'apple'
-  },
-  {
-    num: 4,
-    str: 'cabbage'
-  },
-  {
-    num: 7,
-    str: 'banana'
-  }
-];
+const x = [1,2,3];
+x[-1] = -1;
+x[-4] = 4;
+console.log(x[x.indexOf(10000)]);
+console.log(x)
+/////
 
-arr.sort(function(val1, val2){
-  if(val1.num > val2.num){
-    return -1;
-  }else{
-    return 1;
-  };
-});
+const ary = [1,2,3,15,30,5,45,7];
+console.log(ary.sort((a, b) => {
+  return a > b;
+}))
 
-let arr2 = [...arr];
-arr2.sort(function(val1, val2){
-  if(val1.str < val2.str){
-    return -1;
-  }else{
-    return 1;
-  };
-})
-console.log(arr)
-console.log(arr2)
+/////
+
+let i = Number.MIN_VALUE;
+console.log(i)
+console.log(i * i) // 0
+console.log(i + 1) // +1;
+console.log(i - 1) // -1;
+console.log(i / i) // 1
+
+////
+
+let y = [1,2,3] + [4,5,6];
+let yy = [...[1,2,3], ...[4,5,6]]
+console.log(y)
+console.log(yy)
+
+
+/////
+console.log(555555555555555555);
+console.log(Number.MAX_SAFE_INTEGER);
+/////
+
+(function(){
+  let a = b = 100;
+})()
+console.log(b);
+//console.log(a);
+
+console.log(NaN === NaN)
+console.log(typeof NaN)
