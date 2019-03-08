7236302d60313be8cf1577b3952274ec16465d56
Nicolas <nicolaskrusser@gmail.com>
2019-03-08 11:31:00 +0100

    minmax :(

--- test.js
+++ test.js
@@ -1,24 +1,16 @@
-let arr = [1,2,3,4,5];
-let sum = 0;
-for(let i = 0; i < arr.length; i++){
-  sum += arr[i];
+const arr = [1,4,3,2];
+
+function maxMin (arr){
+  let result = []
+  let max;
+  let min;
+  let arrSum;
+  arr.sort((a,b)=> a - b);
+  arrSum = arr.reduce((a,b)=> a+b)
+  max = arrSum - arr[0];
+  min = arrSum - Math.max.apply(null, arr);
+  result[0]= min;
+  result[1]= max;
+  return result;
 }
-
-let sum2 = arr.reduce((item, acc) => item + acc)
-console.log(sum + ' ' + sum2)
-
-
-const ary = [0,2,1,6,4,5,7,8,10,9] // 1 number is missing
-
-function finder(ary){
-  let num = null;
-  const arr = ary.sort(function(a, b){return a - b})
-  for(let i = 0; i < arr.length; i++){
-    if ((arr[i+1] - arr[i]) > 1){
-      num = arr[i] + 1;
-    }
-  }
-  return num;
-}
-
-console.log(finder(ary))
+console.log(maxMin(arr))
