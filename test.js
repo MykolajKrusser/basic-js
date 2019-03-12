50c6a739590f6de1449dc34b1cc563b3876e9744
Nicolas <nicolaskrusser@gmail.com>
2019-03-12 12:05:59 +0100

    squareDigits

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
-String.prototype.toJadenCase = function () {
-    strArray = this.split('');
-    if(strArray.length === 0){
-        return null
-    }else{
-        for(let i = 0; i < strArray.length; i++){
-            if(strArray[i] === ' '){
-                strArray[i + 1]= strArray[i + 1].toUpperCase();
-            }
-        }
-        strArray[0] = strArray[0].toUpperCase()
-        return strArray.join('');
-    }
-};
-var str = "How can mirrors be real if our eyes aren't real";
-console.log(str.toJadenCase())
+function squareDigits(num){
+  let arr = num.toString().split('');
+  for(let i = 0; i < arr.length; i++){
+    arr[i] = Math.pow(arr[i], 2)
+  };
+  return Number(arr.join(''));
+}
+console.log(squareDigits(124324))
