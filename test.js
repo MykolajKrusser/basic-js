8b0684df39179d45d5e80aba89fec1bed7b59698
Nicolas <nicolaskrusser@gmail.com>
2019-03-29 09:13:38 +0100

    expandedForm of a number

--- test.js
+++ test.js
@@ -5,4 +5,12 @@ function squareDigits(num){
   };
   return Number(arr.join(''));
 }
-console.log(squareDigits(124324))
+//console.log(squareDigits(124324))
+
+
+function expandedForm(num) {
+  let nums = num
+  console.log(nums.toString().split("").reverse().map((a, i) => a * Math.pow(10 , i)).filter(a => a > 0).reverse().join(" + "))
+}
+
+expandedForm(14258);
