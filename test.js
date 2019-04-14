2ec3fcdb7ea1aafa24ee1cbc1d4db30fc31472b8
Nicolas <nicolaskrusser@gmail.com>
2019-04-14 16:35:20 +0200

    part list

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
-function squareDigits(num){
-  let arr = num.toString().split('');
+function partlist(arr) {
+  let result = [];
+  
   for(let i = 0; i < arr.length; i++){
-    arr[i] = Math.pow(arr[i], 2)
-  };
-  return Number(arr.join(''));
-}
-//console.log(squareDigits(124324))
-
-
-function expandedForm(num) {
-  let nums = num
-  console.log(nums.toString().split("").reverse().map((a, i) => a * Math.pow(10 , i)).filter(a => a > 0).reverse().join(" + "))
+    let subarray = [];
+    if(i === 0){
+      return result
+    }
+    subarray = [arr.slice(0, i).join(' '), arr.slice(i).join(' ')]
+    return result.concat([subarray])
+  }
+  
 }
 
-expandedForm(14258);
+console.log(partlist(["I", "wish", "I", "hadn't", "come"]))
